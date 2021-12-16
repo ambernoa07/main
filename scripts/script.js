deec720cdf7a92ba77c87a507eb18e0fa8416865
ambernoa07 <94411188+ambernoa07@users.noreply.github.com>
2021-12-16 18:07:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+
+
+
+var hamburgerButton = document.querySelector("header img:first-of-type");
+var hetMenu = document.querySelector("nav");
+
+function menuAnimatie() {
+hetMenu.classList.toggle("menuopen");
+}
+
+hamburgerButton.addEventListener("click", menuAnimatie);
+
+
+
+var cattextButton = document.querySelector("main > section:nth-of-type(3) > article:nth-of-type(3) > p:nth-of-type(3)");
+var deBody = document.querySelector ("body");
+
+function maakHerfstig() {
+    deBody.classList.add('autumnbody');
+}
+
+cattextButton.addEventListener('click', maakHerfstig)
